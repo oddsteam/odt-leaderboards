21340ce0367376d6c0b7354f0afd49beca91ee86
zkan <kan@odds.team>
2025-04-22 12:34:06 +0700

    Format code using Black

--- leaderboards/runs/apps.py
+++ leaderboards/runs/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class RunsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'runs'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "runs"
